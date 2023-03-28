e6e5883d76fb0373e34291faa6125b7fa1e15069
YuriiSikorskyi <yur411k@gmail.com>
2023-03-28 09:21:27 +0200

    dz

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryCards = createGaleryCards(galleryItems);
+
+galleryEl.addEventListener('click', onGalleryCardclick);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryCards);
+
+function createGaleryCards(galleryItems) {
+    return galleryItems
+        .map(({description, original, preview}) => {
+            return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+            </div> 
+            `;
+        })
+        .join('');
+}
+
+function onGalleryCardclick(e) {
+    console.log(e.target);
+}
+
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
